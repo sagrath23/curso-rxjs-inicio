d207346952ec90395bcd8b1cd64309b9faf11e33
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2020-03-19 08:27:44 -0500

    basic example of observable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+import { Observable } from 'rxjs';
 
+// one way to create a observable 
+//const obs$ = Observable.create();
+const obs$ = new Observable((subs) => {
+  // this method publish a value to all who is subscribed to
+  subs.next('Hello');
+  subs.next('World');
 
-console.log('Hola Mundo!');
+  // and this method notify to all observers that this observable can be unsubscribed
+  // this observable can emit more values with next, but all observers will ignore it.
+  subs.complete();
+
+  subs.next('World');
+});
+
+//subscribe to observable 
+obs$.subscribe(console.log);
 
 
 
